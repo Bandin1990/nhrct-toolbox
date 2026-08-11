cba762eead8cd5a1f8cef539e62ec13e9dbe9eaf
Bandin1990 <bandit.nhrc@gmail.com>
2026-08-11 22:38:10 +0700

    Use a Vercel-compatible request handler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
 const server = require('../server');
 
-module.exports = server;
+// Vercel expects a request handler function. The local server remains
+// reusable, but must not be exported as an http.Server instance directly.
+module.exports = (req, res) => server.emit('request', req, res);
